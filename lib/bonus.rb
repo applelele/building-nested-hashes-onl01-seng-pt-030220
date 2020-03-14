71bf5804a64f493f4535a236095456c41000fe86
YUKIKO IKEDA <applelele@gmail.com>
2020-03-14 16:00:22 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,10 +19,11 @@ def bonus
       ]
    }
   }
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:montague][:heroine][:status] = "dead"
+
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:montague][:heroine][:status] = "dead"
